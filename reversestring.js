2a770a5f19f57c1e895f47d9164a5ad76765eddf
Rachel Burke <Rachel.Burke@experian.com>
2019-08-15 15:58:40 -0400

    Add more testing suites to test code from javascript cardio solutions'

---
+++ reversestring.js
@@ -0,0 +1,13 @@
+const reverseString = str =>
+  str
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
+
+// return str
+//   .split('')
+//   .reverse()
+//   .join('');
+
+module.exports = reverseString;
